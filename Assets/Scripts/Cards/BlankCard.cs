e264177cdbda65510d6c8d358244c80d76445f76
LaraPaul <lapaul@stud.mediadesign.de>
2017-08-23 09:46:37 +0200

    added Shine Animation to Blankcards

--- Assets/Scripts/Cards/BlankCard.cs
+++ Assets/Scripts/Cards/BlankCard.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 namespace Cards {
     public class BlankCard : Card {
         GameObject OwnGO;
+        bool cardprocessdone;
 
         private void Start() {
             OwnGO = GameObject.Find(Slave.GetCardName(cardid, x, y));
-            GameObject F = GameObject.Find("Field");
-            if (F.GetComponent<GameManager>().currentChoosedCard != CardID.Doublecard) {
-            F.GetComponent<GameManager>().animationDone = true;
+            F = GameObject.Find("Field");
+
+            SetAnimationStart();
+        }
+
+        void Update() {
+            if (cardprocessdone) return;
+
+            if (IsSetAnimationEnd()) {
+                cardprocessdone = true;
+                if (F.GetComponent<GameManager>().currentChoosedCard != CardID.Doublecard) {
+                    AnimationDone();
+                }
+
             }
         }
     }
