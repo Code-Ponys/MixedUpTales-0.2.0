5fd97c71b2fb8f54375326ead01b99b538fbcb13
LaraPaul <lapaul@stud.mediadesign.de>
2017-08-24 12:47:46 +0200

    Added HotPotatoe Animation l Changed UI_Game to Camera Overlay
    added new UI_Animations Cam

--- Assets/Scripts/SideBarMove.cs
+++ Assets/Scripts/SideBarMove.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Spine.Unity;
 
 public class SideBarMove : MonoBehaviour {
 
     public bool panelactive;
     public Image CardPreview;
     public Text CardPreviewText;
+    bool AnimationPlayed;
+
+    GameObject An_Hot;
+
+    AudioSource Sound;
+    SkeletonAnimation skeletonAnimation;
+
+    Spine.AnimationState AS;
 
     private void Start() {
+
+        AnimationPlayed = false;
         RectTransform rectTransform = GetComponent<RectTransform>();
         rectTransform.anchoredPosition = new Vector3(240, 0, 0);
     }
@@ -39,5 +50,23 @@ public class SideBarMove : MonoBehaviour {
         panelactive = true;
         CardPreview.enabled = false;
         CardPreviewText.enabled = false;
+
+        if (GameObject.Find("Field").GetComponent<GameManager>().lastSetCard == CardID.HotPotatoe && AnimationPlayed == false) {
+            An_Hot = (GameObject)Instantiate(Resources.Load("Animations/AN_Hot"));
+
+            //Sound = GameObject.Find("ErrorSound (1)").GetComponent<AudioSource>();
+            skeletonAnimation = An_Hot.GetComponent<SkeletonAnimation>();
+            AS = skeletonAnimation.state;
+
+            AS.Complete += delegate {
+                
+                GameObject.Find("Field").GetComponent<GameManager>().animationDone = true;
+                Destroy(An_Hot);
+                AnimationPlayed = true;
+                
+            };
+
+
+        }
     }
 }
